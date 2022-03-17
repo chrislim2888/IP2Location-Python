a6ace87ee2973208562257222b82d42a7160d8ed
IP2Location <support@ip2location.com>
2022-03-17 14:36:08 +0800

    Updated README.md.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IP2Location 8.7.0
+# IP2Location 8.7.1
 
 
 This is a IP2Location Python library that enables the user to find the country, region or state, city, latitude and longitude, ZIP code, time zone, Internet Service Provider (ISP) or company name, domain name, net speed, area code, weather station code, weather station name, mobile country code (MCC), mobile network code (MNC) and carrier brand, elevation, usage type, address type and IAB category by IP address or hostname originates from. The library reads the geo location information from **IP2Location BIN data** file.
@@ -10,8 +10,7 @@ For more details, please visit:
 
 # Requirements
 
-1. Python 2.2 and above for version below than 8.7.0.
-2. Python 3.4 and above for version 8.7.0 and above.
+1. Python 3.5 and above.
 
 # Installation
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="IP2Location",
-	version="8.7.0", 
+	version="8.7.1", 
 	author="IP2Location",
 	author_email="support@ip2location.com",
 	description="This is an IP geolocation library that enables the user to find the country, region, city, latitude and longitude, ZIP code, time zone, ISP, domain name, area code, weather info, mobile info, elevation, usage type, address type and IAB category from an IP address. It supports both IPv4 and IPv6 lookup.",
@@ -19,7 +19,13 @@ setuptools.setup(
 		"Development Status :: 5 - Production/Stable",
 		"Intended Audience :: Developers",
 		"Topic :: Software Development :: Libraries :: Python Modules",
-		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Programming Language :: Python :: 3.10",
+		"Programming Language :: Python :: 3.11",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
