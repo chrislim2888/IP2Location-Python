8864c1711220d28623d9d44a3822d5c9addf32b1
ip2location <support@ip2location.com>
2023-08-22 08:41:11 +0800

    Update to use other theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,9 +19,10 @@ extensions = [
     'sphinx.ext.doctest',
     # 'sphinx.ext.autodoc',
     # 'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    # 'sphinx.ext.intersphinx',
     'myst_parser',
     'sphinx_copybutton',
+    # "sphinxext.opengraph",
 ]
 
 # https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
@@ -48,28 +49,38 @@ myst_enable_extensions = [
 myst_html_meta = {
     # "description lang=en": "metadata description",
     # "description lang=fr": "description des métadonnées",
-    "keywords": "Sphinx, MyST",
+    "description": "IP2Location Python library enables user to query the geolocation information for an IP address.",
+    "keywords": "IP2Location, Geolocation, IP location",
     "google-site-verification": "DeW6mXDyMnMt4i61ZJBNuoADPimo5266DKob7Z7d6i4",
     # "property=og:locale":  "en_US"
 }
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+# OpenGraph metadata
+ogp_site_url = "https://ip2location-python.readthedocs.io/en/latest"
+
+# ogp_custom_meta_tags = [
+    # '<meta name="description" content="IP2Location Python library enables user to query the geolocation information for an IP address.">',
+# ]
+
+# intersphinx_mapping = {
+    # 'python': ('https://docs.python.org/3/', None),
+    # 'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+# }
+# intersphinx_disabled_domains = ['std']
 
 # templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_pdj_theme'
-html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
+html_theme = 'sphinx_book_theme'
 
 # PDJ theme options, see the list of available options here: https://github.com/jucacrispim/sphinx_pdj_theme/blob/master/sphinx_pdj_theme/theme.conf
 html_theme_options = {
-    # Hide the name of the project above the search bar
-    'home_link': 'hide'
+    "use_edit_page_button": False,
+    "use_source_button": False,
+    "use_issues_button": False,
+    "use_download_button": False,
+    "use_sidenotes": False,
 }
 
 # The name of an image file (relative to this directory) to place at the top
@@ -81,4 +92,6 @@ html_favicon = 'images/favicon.ico'
 
 html_title = "IP2Location Python"
 
-html_extra_path = ["googlead5f0f82eb100b8b.html"]
+# html_extra_path = ["googlead5f0f82eb100b8b.html"]
+
+html_baseurl = "https://ip2location-python.readthedocs.io/en/latest/"
